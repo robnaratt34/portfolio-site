67d33694022fc81c9a005c18e9a6b3ef12f41037
John <robnaratt34@gmail.com>
2025-01-16 14:43:50 -0700

    Added MUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
-      
+      <Button variant="contained">Hello world</Button>
     </div>
   );
 }
