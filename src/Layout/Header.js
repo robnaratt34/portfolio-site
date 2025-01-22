c545d37c6f748ca18ebe35e8973193508dd55926
robnaratt34 <56413734+robnaratt34@users.noreply.github.com>
2025-01-22 01:05:26 -0700

    Create Header.js

---
+++ src/Layout/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+
+function Header() {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Header
