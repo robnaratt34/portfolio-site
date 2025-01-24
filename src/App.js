fdf704989e6bf2f09b5a8befc203f661050a3c7a
robnaratt34 <56413734+robnaratt34@users.noreply.github.com>
2025-01-23 17:10:13 -0700

    Updated App.js to pull app data

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Button from '@mui/material/Button';
 import ProjectView from './Layout/ProjectView';
 import Header from './Layout/Header';
 import HeaderBar from './Layout/HeaderBar';
+import {data} from './data/data';
 
 function App() {
+  const {projects, about, skills, acheivements, contact} = data
   return (
     <div className="App">
       <Header />
       <HeaderBar />
-      <ProjectView />
+      <ProjectView projects={projects} />
     </div>
   );
 }
