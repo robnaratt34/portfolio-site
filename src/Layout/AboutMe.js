19208e623cf7755583fa4a92bf0417e145deef7c
robnaratt34 <56413734+robnaratt34@users.noreply.github.com>
2025-01-24 20:01:36 -0700

    Update AboutMe.js to include image and description

--- src/Layout/AboutMe.js
+++ src/Layout/AboutMe.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Box, Container} from "@mui/material";
+import profile from "../Media/js.png"
 
-function AboutMe(){
+function AboutMe({about, skills, achievements}){
   return (
-
+    <Box sx={{display:"flex"}}>
+      <Box component="img", src={profile} />
+      <Typography variant="body1">
+        About Me goes here
+      </Typography>
+    </Box>
   )
 }
 
