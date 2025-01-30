815aec25b6d8de3fc5713fa6c808ee82a6596b2b
John <robnaratt34@gmail.com>
2025-01-30 14:43:58 -0700

    Added theme to entire website

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import HeaderBar from './Layout/HeaderBar';
 import AboutMe from './Layout/AboutMe';
 import ContactMe from './Layout/ContactMe';
 import {data} from './data/data';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './Theme/theme';
 
 function App() {
   const {projects, about, skills, acheivements, contact} = data
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
       <HeaderBar />
       <ProjectView projects={projects} />
       <AboutMe about={about} skills={skills} achievements={acheivements} />
       <ContactMe contact={contact} />
     </div>
+    </ThemeProvider>
   );
 }
 
